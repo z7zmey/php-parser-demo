a5edd54ce75bcf7fd2c7d9ff2adbb3a01d785f74
z7zmey <z7zmey@gmail.com>
2018-04-11 00:24:39 +0300

    parser update

--- app.go
+++ app.go
@@ -7,57 +7,67 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/z7zmey/php-parser/comment"
-	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/parser"
 	"github.com/z7zmey/php-parser/php5"
 	"github.com/z7zmey/php-parser/php7"
 	"github.com/z7zmey/php-parser/position"
 	"github.com/z7zmey/php-parser/visitor"
 )
 
+var port *int
+
 func main() {
+
+	var port = flag.Int("port", 8080, "listen port")
+	flag.Parse()
 
 	binDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
 	http.HandleFunc("/parse", parseHandler)
 	http.Handle("/", http.FileServer(http.Dir(binDir+"/www")))
 	http.HandleFunc("/_ah/health", healthCheckHandler)
-	log.Print("Listening on port 80")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Printf("Listening on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
 func parseHandler(w http.ResponseWriter, r *http.Request) {
-	var nodes node.Node
+	var p parser.Parser
 	var comments comment.Comments
 	var positions position.Positions
 
 	src := bytes.NewBufferString(r.FormValue("script"))
-	isPhp5 := r.FormValue("php5")
-	isShowPositions := r.FormValue("positions")
-	isShowComments := r.FormValue("comments")
 
-	if isPhp5 == "true" {
-		nodes, comments, positions = php5.Parse(src, "input.php")
+	if r.FormValue("php5") == "true" {
+		p = php5.NewParser(src, "input.php")
 	} else {
-		nodes, comments, positions = php7.Parse(src, "input.php")
+		p = php7.NewParser(src, "input.php")
 	}
 
-	if isShowPositions == "false" {
-		positions = nil
+	p.Parse()
+
+	if r.FormValue("positions") == "true" {
+		positions = p.GetPositions()
 	}
 
-	if isShowComments == "false" {
-		comments = nil
+	if r.FormValue("comments") == "true" {
+		comments = p.GetComments()
+	}
+
+	for _, e := range p.GetErrors() {
+		io.WriteString(w, e.String()+"\n")
 	}
 
 	nsResolver := visitor.NewNamespaceResolver()
-	nodes.Walk(nsResolver)
+	p.GetRootNode().Walk(nsResolver)
 
 	dumper := visitor.Dumper{
 		Writer:     w,
@@ -66,7 +76,7 @@ func parseHandler(w http.ResponseWriter, r *http.Request) {
 		Positions:  positions,
 		NsResolver: nsResolver,
 	}
-	nodes.Walk(dumper)
+	p.GetRootNode().Walk(dumper)
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
